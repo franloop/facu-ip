8e4dbe935c3962579f50b3357d80dbfeaaaa353b
Franco G. <francoeg95@gmail.com>
2025-05-02 00:49:43 -0300

    borro archivos duplicados

--- p4.hs
+++
@@ -1,6 +0,0 @@
-sumaPotenciasAux :: (Integer,Integer) -> Integer
-sumaPotenciasAux (q,1) = q
-sumaPotenciasAux (q,x) = (q ^ x) * (sumaPotenciasAux (q,(x-1)))
-
-sumaPotencias :: Integer -> Integer -> Integer -> Integer
-sumaPotencias q n m = (sumaPotenciasAux (q,n)) * (sumaPotenciasAux (q,m))

271e7ed89538b8262069cc7b0a89c88a10bee823
Franco G. <francoeg95@gmail.com>
2025-05-02 02:47:01 -0300

    pruebo otra forma sin aplanar el tablero

--- integradores/sopa.hs
+++ integradores/sopa.hs
@@ -37,12 +37,237 @@ requiere: {El tablero t no es vac´ıo, todos los n´umeros del tablero son posi
 asegura: {res es igual al n´umero que m´as veces aparece en un tablero t. Si hay empate devuelve cualquiera de ellos}
 }
 
+
+masRepetido:
+    tiene que recibir un tablero
+    tiene que poder encontrar el elemento mas repetido en ese tablero
+
+    depende de:
+
+    recorrer las filas y columnas
+    almacenar estado del elemento mas repetido y sus repeticiones
+
+    termina si: 
+    el tablero es vacio -> 0
+    la busqueda finaliza -> devuelve mas repetido
+
+funcion:
+    recibe:
+    devuelve:
+    necesita:
+    debe:
+    depende:
+    termina si:
+
+buscar:
+    recibe:
+        tablero
+        elemento mas repetido
+        repeticiones
+
+    devuelve 
+        numero mas repetido
+    
+    necesita: 
+        conocer cual es el maximo encontrado y su repeticiones maximas
+    
+    debe:
+        1- contar un elemento en el tablero 
+
+        2- comparar ese valor con el de la maxima maxima repeticion del elemento 
+        que mas apariciones tenga de buscar en el tablero sin ese elemento
+
+        SI
+            encontre el mas repetido.
+        sino
+            el mas repetido esta dentro del tablero
+            
+    depende: 
+        contar un elemento en el tablero
+        borrar algun elemento del tablero
+
+    termina si: 
+        el tablero es vacio -> numero más repetido
+        el tablero no es vacio -> sigue buscando
+
+
+-}
+
+-- buscar :: tablero -> masRepetidoParcial -> cantVeces -> masRepetido
+buscarRepetido :: Tablero -> Integer -> Integer -> Integer
+buscarRepetido [] n _ = n
+buscarRepetido tablero mrp cv | contarPrimerElemento >= cv = buscarEnTableroReducido
+                              | otherwise = buscarRepetido tableroReducido mrp cv
+                    where primerElemento = head ( head tablero)
+                          tableroReducido = limpiarListasVacias (borrarElementoTablero tablero primerElemento)
+                          buscarEnTableroReducido = buscarRepetido tableroReducido primerElemento contarPrimerElemento
+                          contarPrimerElemento = contarElementoTablero tablero primerElemento
+
+limpiarListasVacias :: Tablero -> Tablero
+limpiarListasVacias [] = []
+limpiarListasVacias (lista:tablero) | lista == [] = limpiarListasVacias tablero
+                                 | otherwise = lista:limpiarListasVacias tablero
+
+
+-- buscarRepetido [ [2,3,6,3,10,13,2] ] , 0 , 0 -> 2 > 0 = buscarRepetido [[3,6,3,10,13]], 2, 2
+-- buscarRepetido [[3,6,3,10,13]], 2, 2 -> 2 >= 2 = buscarRepetido [[6,10,13]], 3, 2
+-- buscarREpetido [[6,10,13]], 3, 2 -> 2 >= 1 != buscarRepetido 
+
+
+-- TESTING --          
+getFila :: Integer -> Fila
+getFila 1 = [3,3,3,3,6,10,33,2 ,3, 6, 3]
+getFila 2 = [2,1,6,2,10,33,3,2]
+getFila 3 = [2,23,2,6,2,1,4,5,10,38,2]
+getFila 4 = [2,3,6,3,10,13,2]
+
+getTablero :: Integer -> Tablero
+getTablero 1 = (getFila 1):(getFila 2):(getFila 2):(getFila 4):[]
+getTablero 2 = (getFila 2):(getFila 3):(getFila 4):[]
+getTablero 3 = (getFila 3):(getFila 4):[]
+getTablero 4 = (getFila 4):[]
+getTablero 5 = []
+
+
+{-
+funcion:
+    recibe:
+    devuelve:
+    necesita:
+    debe:
+    depende:
+    termina si:
+
+borrarElementoTablero:
+    recibe:
+        tablero
+        elemento a borrar
+    
+    devuelve:
+        tablero sin elemento a borrar
+
+    necesita: 
+        recorrer las filas de un tablero y borrar elemento
+    
+    debe: 
+        eliminar el elemento de cada fila
+        juntar las filas sin el elemento
+    
+    depende:
+        de eliminar elementos en filas
+    
+    termina si:
+        el tablero es vacio -> []
+        el tablero no es vacio -> sigue iterando las filas.
+-}
+
+borrarElementoTablero :: Tablero -> Integer -> Tablero
+borrarElementoTablero [] _ = []
+borrarElementoTablero (fila:tablero) elemento = (borrarElementoFila fila elemento):borrarElementoTablero tablero elemento 
+
+
+{-
+funcion:
+    recibe:
+    devuelve:
+    necesita:
+    debe:
+    depende:
+    termina si:
+
+borrarElementoFila:
+    recibe:
+        Fila
+        elemento
+    devuelve:
+        Fila sin elemento
+    necesita:
+        recorrer fila 
+    debe: 
+       comparar un elemento X de la fila con el elemento a borrar. 
+    depende:
+        nada
+    termina si:
+        fila es vacio -> []
+        fila no es vacio  -> compara el siguiente
+-}
+
+borrarElementoFila :: Fila -> Integer -> Fila
+borrarElementoFila [] _ = []
+borrarElementoFila (x:fila) elemento | x == elemento = borrarElementoFila fila elemento
+                                     | otherwise = x:(borrarElementoFila fila elemento)
+
+{-
+
+contarElementoTablero:
+    recibe:
+        tablero
+        elemento
+    
+    devuelve:
+        cantidad de veces que aparece elemento en tablero
+
+    debe:
+        recorrer el tablero contando la aparicion de elemento en todas sus filas
+    
+    depende:
+        de contar elementos en una fila. 
+
+    termina si:
+        el tablero es vacio -> 0
+        recorre todas las filas -> devuelve la cantidad de veces que aparece elemento
+-}
+
+contarElementoTablero :: Tablero -> Integer -> Integer
+contarElementoTablero [] _ = 0
+contarElementoTablero (fila:tablero) elemento = contarElementoFila fila elemento + contarElementoTablero tablero elemento
+
+
+{-
+contarElementoFila
+    recibe:
+        fila
+        elemento
+    
+    devuelve:
+        cantidad de veces que aparece elemento en fila
+
+    debe:
+        tomar un elemento de la fila y contar sus apariciones. 
+    
+    depende:
+        de contar el elemento -> puedo hacerlo. 
+
+    termina si:
+        la fila es vacia -> 0
+        recorre todos los elementos -> devuelve la cantidad de veces que aparece elemento
+-}
+
+contarElementoFila :: Fila -> Integer -> Integer
+contarElementoFila [] _ = 0
+contarElementoFila (e:resto) elemento |  e == elemento = 1 + contarElementoFila resto elemento
+                                     | otherwise = contarElementoFila resto elemento
+
+
+
+
+
+    
+
+
+
+
+
+{-
+
+
 masRepetido :: Tablero -> Integer
 masRepetido [] = 0
 masRepetido (fila:tablero) | contarElementoTablero tableroCompleto primerElementoFila >= contarElementoTablero (borrarElementoTablero tableroCompleto primerElementoFila) (masRepetido tableroCompleto) = primerElementoFila
                            | otherwise = masRepetido tablero
                            where tableroCompleto = fila:tablero
                                  primerElementoFila = head fila--}
+{- 
 -- Si convierto toda la matriz en una fila, puedo buscar facilmente cual es el repetido. 
 masRepetido :: Tablero -> Integer
 masRepetido [] = 0
@@ -70,29 +295,8 @@ borrarElementoFila :: Fila -> Integer -> Fila
 borrarElementoFila [] _ = []
 borrarElementoFila (e:fila) elemento | e == elemento = borrarElemento
                                      | otherwise = e:borrarElemento
-                                where borrarElemento = borrarElementoFila fila elemento
-
-{-- 
-contarElementoTablero :: Tablero -> Integer -> Integer
-contarElementoTablero [] _ = 0
-contarElementoTablero (fila:tablero) elemento = contarElementoFila fila elemento + contarElementoTablero tablero elemento 
-
-borrarElementoTablero :: Tablero -> Integer -> Tablero
-borrarElementoTablero [] _ = []
-borrarElementoTablero (fila:tablero) elemento = (borrarElementoFila fila elemento):borrarElementoTablero tablero elemento  
---}
+                                where borrarElemento = borrarElementoFila fila elemento -}
 
 
--- TESTING --          
-getFila :: Integer -> Fila
-getFila 1 = [3,3,3,3,6,10,33,2 ,3, 6, 3]
-getFila 2 = [2,1,6,2,10,33,3,2]
-getFila 3 = [2,23,2,6,2,1,4,5,10,38,2]
-getFila 4 = [2,3,6,3,10,13,2]
 
-getTablero :: Integer -> Tablero
-getTablero 1 = (getFila 1):(getFila 2):(getFila 3):(getFila 4):[]
-getTablero 2 = (getFila 2):(getFila 3):(getFila 4):[]
-getTablero 3 = (getFila 3):(getFila 4):[]
-getTablero 4 = (getFila 4):[]
-getTablero 5 = []
+
